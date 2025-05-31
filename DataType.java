91e3506b5baeaeaf40d2dd030c1b3259c688dd2f
Tanvirahme2024 <ta7142047@gmail.com>
2025-06-01 04:50:39 +0600

    Java Practice

---
+++ DataType.java
@@ -0,0 +1,19 @@
+public class DataType {
+    public static void main(String[] args) {
+        int num1=5;
+        byte by=127;
+        short sh=558;
+        long l=121312l;
+
+        float f=33.3f;
+        // double=54646.565;
+
+        char c='T';
+        boolean bool=true;
+
+       
+
+
+    }
+    
+}
---
+++ hello.java
@@ -0,0 +1,17 @@
+class hello
+{
+    public static void main(String a[])
+    {
+        System.out.println("Tanvir Mithila");
+        System.out.println(9+6);
+
+        int num=155;
+        System.out.println(num);
+
+        double num1=1.4;
+        System.out.println(num1);
+
+        float num2=1.4f;
+        System.out.println(num2);
+    }
+}
