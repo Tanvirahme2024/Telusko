6a5306d75d4cbf518cffa37cd153a9cd13050ac5
Tanvirahme2024 <ta7142047@gmail.com>
2025-06-26 23:20:00 +0600

    practice Class and method

--- practice_class.java
+++ practice_class.java
@@ -1,18 +1,39 @@
 class calculator {
-    public int add(int num1,int num2)
+    int add(int a,int b)
     {
-        int result = num1 + num2;
-        return result;
+        int sum=a+b;
+        return sum;
     }
+    int sub(int a,int b)
+    {
+        int sub=a-b;
+        return sub;
+    }
+    double div(int a,int b)
+    {
+        if(b==0)
+        {
+            System.out.println("Division by zero is not allowed.");
+            return 0;
+        }
+        double div=a/b;
+        return div;
+    }
+
+    
 }
 
-public class practice_class {
-    public static void main(String[] args)
-    {
-        calculator calc=new calculator();
-        int sum =calc.add(5,20);
-        System.out.println(sum);
 
+public class practice_class {
+    public static void main(String[] args)    
+    {
+        calculator cal=new calculator();
+        int sum=cal.add(10,200);
+        System.out.println(sum);
+        int sub=cal.sub(1500,20);
+        System.out.println(sub);
+        double div=cal.div(100,0);
+        System.out.println(div);;
     }
     
 }
